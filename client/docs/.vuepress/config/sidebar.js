e261db2f743d943a9b5a3ef0d57a5f4a51e26f00
tianyan071128 <1033051985@qq.com>
2021-10-22 17:45:15 +0800

    HTML - 全局属性

--- client/docs/.vuepress/config/sidebar.js
+++ client/docs/.vuepress/config/sidebar.js
@@ -1,17 +1,30 @@
 // 侧边栏
 module.exports = {
   // 路径配置
-  '/01_前端/': [
+  "/01_前端/": [
     {
-      title: 'html', // 分组标题
-      collapsable: true, // 是否可折叠，可选的，默认true
+      title: "基础标签", // 分组标题
+      collapsable: false, // 是否可折叠，可选的，默认true
       sidebarDepth: 2, // 深度，可选的, 默认值是 1
       children: [
         // 这个表示的是路径?
-        '01_html/01_index',
-        '01_html/02_head',
-        '01_html/03_meta',
-      ]
+        "01_html/01_index",
+        "01_html/02_全局属性",
+        "01_html/03_head",
+        "01_html/04_meta",
+      ],
     },
+    // {
+    //   title: "表单", // 分组标题
+    //   collapsable: false, // 是否可折叠，可选的，默认true
+    //   sidebarDepth: 2, // 深度，可选的, 默认值是 1
+    //   children: [
+    //     // 这个表示的是路径?
+    //     "01_html/01_index",
+    //     "01_html/02_全局属性",
+    //     "01_html/03_head",
+    //     "01_html/04_meta",
+    //   ],
+    // },
   ],
-}
+};
