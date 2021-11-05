044e7992354208dda9af3471634386683b0ce57f
wenzubiao <1033051985@qq.com>
2021-11-05 23:00:49 +0800

    上送服务器

--- client/docs/.vuepress/components/httpTest.vue
+++ client/docs/.vuepress/components/httpTest.vue
@@ -7,17 +7,49 @@
 -->
 <template>
   <div class="http-test">
-    <template v-if="type === 'cache'">cacheControl
-      <el-button type="primary" @click="sendGET">发送 GET 请求, 会缓存</el-button>
-      <el-button type="primary" @click="sendPOST">发送 POST 请求, 不会缓存</el-button>
+    <template v-if="type === 'cache'"
+      >cacheControl
+      <el-button type="primary" @click="sendGET"
+        >发送 GET 请求, 会缓存</el-button
+      >
+      <el-button type="primary" @click="sendPOST"
+        >发送 POST 请求, 不会缓存</el-button
+      >
     </template>
-    <el-button v-else-if="type === 'head'" type="primary" @click="sendHEAD">发送 HEAD 请求, 响应体不会接收到</el-button>
-    <el-button v-else-if="type === 'status'" type="primary" @click="sendStatus">发送请求, 查看状态码</el-button>
-    <el-button v-else-if="type === 'Encoding'" type="primary" @click="sendEncoding">发送请求, 返回 gzip 压缩数据</el-button>
-    <el-button v-else-if="type === 'Pragma'" type="primary" @click="sendPragma">发送 GET 请求, 当同时存在 Cache-Control 和 Pragma, 不会进行强缓存</el-button>
-    <el-button v-else-if="type === 'cacheControl'" type="primary" @click="sendGET">只存在 Cache-Control 时, 会进行强缓存, </el-button>
-    <el-button v-else-if="type === '简单请求'" type="primary" @click="sendJiandan">发送简单请求 </el-button>
-    <el-button v-else-if="type === '非简单请求'" type="primary" @click="sendFeiJiandan">发送非简单请求 </el-button>
+    <el-button v-else-if="type === 'head'" type="primary" @click="sendHEAD"
+      >发送 HEAD 请求, 响应体不会接收到</el-button
+    >
+    <el-button v-else-if="type === 'status'" type="primary" @click="sendStatus"
+      >发送请求, 查看状态码</el-button
+    >
+    <el-button
+      v-else-if="type === 'Encoding'"
+      type="primary"
+      @click="sendEncoding"
+      >发送请求, 返回 gzip 压缩数据</el-button
+    >
+    <el-button v-else-if="type === 'Pragma'" type="primary" @click="sendPragma"
+      >发送 GET 请求, 当同时存在 Cache-Control 和 Pragma,
+      不会进行强缓存</el-button
+    >
+    <el-button
+      v-else-if="type === 'cacheControl'"
+      type="primary"
+      @click="sendGET"
+      >只存在 Cache-Control 时, 会进行强缓存,
+    </el-button>
+    <el-button
+      v-else-if="type === '简单请求'"
+      type="primary"
+      @click="sendJiandan"
+      >发送简单请求
+    </el-button>
+    <el-button
+      v-else-if="type === '非简单请求'"
+      type="primary"
+      @click="sendFeiJiandan"
+      >发送非简单请求
+    </el-button>
   </div>
 </template>
 
@@ -33,7 +65,7 @@ export default {
       this.$http.get('/http/cache');
     },
     sendPOST() {
-      this.$http.post('/http/cache', {test: '123'});
+      this.$http.post('/http/cache', { test: '123' });
     },
     sendHEAD() {
       this.$http.head('/http/cache');
@@ -51,14 +83,18 @@ export default {
       this.$http.get(`${process.env.BASE_URL}/http/cors`);
     },
     sendFeiJiandan() {
-      this.$http.put(`${process.env.BASE_URL}/http/cors`, {}, { headers: {'X-Requested-With': 'XMLHttpRequest'} });
-    }
-  }
-}
+      this.$http.put(
+        `${process.env.BASE_URL}/http/cors`,
+        {},
+        { headers: { 'X-Requested-With': 'XMLHttpRequest' } }
+      );
+    },
+  },
+};
 </script>
 
-<style  scoped>
-  .http-test {
-    margin: 15px 0;
-  }
+<style scoped>
+.http-test {
+  margin: 15px 0;
+}
 </style>
