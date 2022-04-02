cdc00358142424c50c6fc8460bfa7d6f1210b31a
tianyan071128 <1033051985@qq.com>
2022-04-02 15:19:34 +0800

    webpack03

--- client/demo/webpack/00_process/.eslintrc.js
+++ client/demo/webpack/00_process/.eslintrc.js
@@ -4,9 +4,10 @@ module.exports = {
     node: true,
     browser: true,
     es2020: true,
+    sourceType: 'module',
   },
   // 使用 eslint-plugin-vue 中 essential 配置 and eslint 默认 recommended 配置
   extends: ['eslint:recommended'],
   rules: {},
-  parser: '@babel/eslint-parser',
+  // parser: '@babel/eslint-parser',
 };
