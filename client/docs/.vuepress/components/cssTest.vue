5856a6a0c5f855cd04b2cf94573676530712f254
tianyan071128 <1033051985@qq.com>
2022-04-08 17:34:27 +0800

    css-伪类和伪元素

--- client/docs/.vuepress/components/cssTest.vue
+++ client/docs/.vuepress/components/cssTest.vue
@@ -1,9 +1,33 @@
-<template></template>
+<template>
+  <div>
+    <template v-if="type === '::selection'">
+      <div class="test-selection">
+        当你高亮显示时，这个文本有特殊的样式.
+        <p>也试着在这个段落中选择文本.</p>
+      </div>
+    </template>
+  </div>
+</template>
 
 <script>
 export default {
   name: 'CssTest',
+  props: {
+    type: String,
+  },
 };
 </script>
 
-<style scoped></style>
+<style scoped>
+/* 选中的文本是红色背景，金黄色的字体 */
+.test-selection::selection {
+  color: gold;
+  background-color: red;
+}
+
+/*选中的是蓝色背景，白色的字体的段落*/
+.test-selection p::selection {
+  color: white;
+  background-color: blue;
+}
+</style>
