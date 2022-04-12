0f80cc74e521f531c7b79947b1b2fb3a62dd5ad0
tianyan071128 <1033051985@qq.com>
2022-04-12 11:55:43 +0800

    css - 弹性布局

--- client/docs/.vuepress/components/cssTest.vue
+++ client/docs/.vuepress/components/cssTest.vue
@@ -6,6 +6,29 @@
         <p>也试着在这个段落中选择文本.</p>
       </div>
     </template>
+    <template v-if="type === ':active'">
+      <div class="test-active">
+        卡片内容：可以利用 :active 伪类在点击卡片时提示一下
+      </div>
+    </template>
+    <template v-if="type === ':focus-within'">
+      <form class="focus-within">
+        <label for="given_name">Given Name:</label>
+        <input id="given_name" type="text" />
+        <br />
+        <label for="family_name">Family Name:</label>
+        <input id="family_name" type="text" />
+      </form>
+    </template>
+    <template v-if="type === ':link'">
+      <p class="link">
+        这一段包含一个链接:
+        <a href="#链接伪类-link-和-visited">
+          当你点击这个链接时，它会变成红色.
+        </a>
+        当你点击该段或链接时，该段将得到一个灰色的背景.
+      </p>
+    </template>
   </div>
 </template>
 
@@ -30,4 +53,40 @@ export default {
   color: white;
   background-color: blue;
 }
+.test-active {
+  padding: 15px;
+  border: 1px solid rgba(0, 0, 0, 0.1);
+  width: 200px;
+  margin-left: 20px;
+  cursor: pointer;
+}
+.test-active:active {
+  background-color: rgba(0, 0, 0, 0.1);
+}
+
+.focus-within {
+  border: 1px solid;
+  color: gray;
+  padding: 4px;
+}
+.focus-within:focus-within {
+  background: #ff8;
+  color: black;
+}
+.link a:link {
+  color: blue;
+}
+.link a:visited {
+  color: purple;
+} /* 已访问链接 */
+.link a:hover {
+  background: yellow;
+} /* 用户鼠标悬停 */
+.link a:active {
+  color: red;
+} /* 激活链接 */
+
+.link:active {
+  background: #eee;
+} /* 激活段落 */
 </style>
