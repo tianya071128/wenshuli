ae63973634f9cb10ac9b90870c31061526995ee6
tianyan071128 <1033051985@qq.com>
2022-06-13 17:20:58 +0800

    HTTPS-TLS 握手阶段抓包分析

--- client/docs/.vuepress/config/sidebar.js
+++ client/docs/.vuepress/config/sidebar.js
@@ -157,7 +157,7 @@ module.exports = {
       title: 'HTTPS', // 分组标题
       collapsable: false, // 是否可折叠，可选的，默认true
       sidebarDepth: 2, // 深度，可选的, 默认值是 1
-      children: ['01_https', '02_https安全性', '03_TLS'],
+      children: ['01_https', '02_https安全性', '03_TLS', '04_抓包分析'],
     },
   ],
   '/06_工程化/01_vscode/': [
